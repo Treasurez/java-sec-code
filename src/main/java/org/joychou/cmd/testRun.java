245db17574c1ccefd62d066e4a6e367e1df0b8e4
zzz <534700646@qq.com>
2024-06-20 10:32:32 +0800

    add test cmd

--- src/main/java/org/joychou/cmd/testRun.java
+++ src/main/java/org/joychou/cmd/testRun.java
@@ -16,25 +16,4 @@ public class testRun {
         System.out.println(result);
     }
 
-//    public static void main(String[] args) throws IOException {
-//        String path = "ls-l; /System/Applications/Calculator.app";
-//        String[] cmd = { "open", path };
-//
-//        Runtime.getRuntime().exec(cmd);
-//    }
-
-//    public static void main(String[] args) throws IOException, InterruptedException {
-//        String path = "/System/Applications/Calculator.app/Contents/MacOS/";
-//        String[] cmd1 = { "ls", "-l", path };
-//
-//        ProcessBuilder pb1 = new ProcessBuilder(cmd1);
-//        Process process1 = pb1.start();
-//        process1.waitFor();
-//
-//        String[] cmd2 = { "open", "/System/Applications/Calculator.app" };
-//
-//        ProcessBuilder pb2 = new ProcessBuilder(cmd2);
-//        Process process2 = pb2.start();
-//        process2.waitFor();
-//    }
 }
